7512df788f0b247f95524501170b22ac678250e0
darylyap <darylyap123@yahoo.com.sg>
2020-11-18 22:46:04 +0800

    updated movement node

--- src/main_ttb/scripts/2d_surface_plot.py
+++ src/main_ttb/scripts/2d_surface_plot.py
@@ -7,44 +7,6 @@ import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float32
 
-
-#Testing for listener
-# def callback_dist(data):
-#     rospy.loginfo(rospy.get_caller_id() + " %.2f ", data.data)
-
-# def callback_time(data):
-#     rospy.loginfo(rospy.get_caller_id() + " %.2f ", data.data)
-
-# def callback_end_message(data):
-#     rospy.loginfo(rospy.get_caller_id() + " %s ", data.data)
-    
-#     # if (data.data == 'end'):
-#     # 	plot_visualisation()
-    
-# def listener():
-
-#     # In ROS, nodes are uniquely named. If two nodes with the same
-#     # name are launched, the previous one is kicked off. The
-#     # anonymous=True flag means that rospy will choose a unique
-#     # name for our 'listener' node so that multiple listeners can
-#     # run simultaneously.
-#     rospy.init_node('listener', anonymous=True)
-#     # rospy.Subscriber("dist_travel", Float32, callback_dist)
-#     rospy.Subscriber("time_travel", Float32, callback_time)
-#     # rospy.Subscriber("end_message", String, callback_end_message)
-
-#     # spin() simply keeps python from exiting until this node is stopped
-#     rospy.spin()
-
-# if __name__ == '__main__':
-#     try:
-#         listener()
-#     except rospy.ROSInterruptException:
-#         pass
-
-
-
-
 def plot_visualisation():
 
 	------------------------------RAW DATA--------------
--- src/main_ttb/scripts/movement_node.py
+++ src/main_ttb/scripts/movement_node.py
@@ -3,9 +3,24 @@
 import rospy
 import math
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String
-from std_msgs.msg import Float32
+from std_msgs.msg import String, Float32
+from turtlesim.msg import Pose
 
+
+def pose_callback(pose):
+    #Theta: -pi<theta<pi
+    rospy.loginfo(rospy.get_caller_id() + " %.2f ", pose.theta)
+
+def xy_callback(data):
+    global temp_array
+    temp_array.append(data)
+
+def segment_array():
+    global temp_array
+    global combi_array
+
+    combi_array.append(temp_array)
+    temp_array = []
 
 def straight(dist, linear_vel, vel_msg, velocity_publisher, distance_publisher, time_publisher):
 
@@ -22,7 +37,7 @@ def straight(dist, linear_vel, vel_msg, velocity_publisher, distance_publisher, 
         current_distance = linear_vel*(t1-t0)
         # distance_publisher.publish(current_distance)
         # time_publisher.publish(t1-t0)
-        print(t1-t0, current_distance)
+        # print(t1-t0, current_distance)
 
     vel_msg.linear.x = 0
 
@@ -45,7 +60,7 @@ def turn_90(turn_right, angular_vel, vel_msg, velocity_publisher, distance_publi
     current_angle = 0
     t0 = rospy.Time.now().to_sec()
 
-    while(current_angle < (math.pi*1.5/2)):
+    while(current_angle < (math.pi/2)):
         velocity_publisher.publish(vel_msg)
         t1 = rospy.Time.now().to_sec()
         current_angle = abs(angular_vel)*(t1-t0)
@@ -59,26 +74,35 @@ def turn_90(turn_right, angular_vel, vel_msg, velocity_publisher, distance_publi
 def move():
     # Starts a new node
     rospy.init_node('movement_node', anonymous=True)
-    velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    # velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)  # to test on turtlesim
+    # velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)  # to test on turtlesim
     # launch with roscore & rosrun turtlesim turtlesim_node & python3 movement_node.py
-    distance_publisher = rospy.Publisher('/dist_travel', Float32, queue_size=10)
-    time_publisher = rospy.Publisher('/time_travel', Float32, queue_size=10)
-    end_message_publisher = rospy.Publisher('/end_message', String, queue_size=10)
+    # distance_publisher = rospy.Publisher('/dist_travel', Float32, queue_size=10)
+    # time_publisher = rospy.Publisher('/time_travel', Float32, queue_size=10)
+    # segment_break_publisher = rospy.Publisher('/segment_break', String, queue_size=10)
+    # end_message_publisher = rospy.Publisher('/end_message', String, queue_size=10)
+
+    rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+    rospy.Subscriber('/xy_pos', Int64MultiArray, xy_callback)
 
     rate = rospy.Rate(1)  # 10hz
     vel_msg = Twist()
 
+    # Init array
+    temp_array = []
+    combi_array = []
+
     # Desired parameters
-    linear_vel = 0.1  # m/s
-    angular_vel = 0.2  # rad/s
-    linear_distance = 0.02  # metres #Straight forward path
-    horizontal_distance = 0.01  # metres #Straight path but for turning
+    linear_vel = 1  # m/s
+    angular_vel = 1  # rad/s
+    linear_distance = 2  # metres #Straight forward path
+    horizontal_distance = 0.5  # metres #Straight path but for turning
 
     # Number of full length travelled as defined by dist = 'linear_distance'
     num_straight_paths = 2
+
     # Direction of first turn after first straight path. Right = True, Left = False
-    turn_right = True
+    turn_right = False
 
     # Init parameters
     vel_msg.linear.x = 0
@@ -103,12 +127,14 @@ def move():
             # Turning
             if (turn_count < desired_turn_count):
                 turn_90(turn_right, angular_vel, vel_msg,velocity_publisher, distance_publisher, time_publisher)
+                # segment_break_publisher.publish('seg_break')
+                segment_array()
                 straight(horizontal_distance, linear_vel, vel_msg, velocity_publisher,distance_publisher, time_publisher)  # Straight path [HORIZONTAL]
+                # segment_break_publisher.publish('seg_break')
+                segment_array()
                 turn_90(turn_right, angular_vel, vel_msg,velocity_publisher, distance_publisher, time_publisher)
                 turn_count += 1
                 turn_right = not turn_right
-                
-            print('TEST HELLO')
 
         vel_msg.linear.x = 0
         velocity_publisher.publish(vel_msg)
@@ -116,6 +142,19 @@ def move():
         print('End of path')
         rate.sleep()
         break
+
+-------------------
+def callback(data):
+    #Theta: -pi<theta<pi
+    global segment
+    if (data == 'seg_break'):
+        segment = True
+
+    if (segment == True):      
+        combi_array.append(temp_array)
+        temp_array = []
+        segment = False
+
 
 
 if __name__ == '__main__':
